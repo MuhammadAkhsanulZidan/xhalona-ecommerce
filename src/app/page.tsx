5be36dea97645ed4c6f83e10661c965a369afc45
Muhammad Akhsanul Zidan <akhsanul.zidan25@gmail.com>
2024-12-02 10:51:54 +0700

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 
 export default function Home() {
   useEffect(() => {
-    window.location.href = "/xhalon.apk";
+    window.location.href = "/Xhalon.apk";
   }, []);
 
   const handleDownloadClick = () => {
