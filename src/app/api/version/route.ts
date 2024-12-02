6e4d031bf42547c35db199dbf813db2f9711ac6f
Muhammad Akhsanul Zidan <akhsanul.zidan25@gmail.com>
2024-12-02 11:24:00 +0700

    m

--- src/app/api/version/route.ts
+++ src/app/api/version/route.ts
@@ -1,5 +1,5 @@
 export async function GET() {
-  return new Response(JSON.stringify({ version: '0.1' }), {
+  return new Response(JSON.stringify({ version: '0.0.1' }), {
     status: 200,
     headers: { 'Content-Type': 'application/json' },
   });
