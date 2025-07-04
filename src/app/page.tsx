1a2874178b0834c793d0e43604ff54b6964b35f8
Muhammad Akhsanul Zidan <akhsanul.zidan25@gmail.com>
2025-07-04 18:17:50 +0700

    u

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,15 @@ import Image from "next/image";
 import { useEffect } from "react";
 
 export default function Home() {
-  useEffect(() => {
-    window.location.href = "/amira.apk";
-  }, []);
+  // useEffect(() => {
+  //   window.location.href = "/amira.apk";
+  // }, []);
 
-  const handleDownloadClick = () => {
+  const handleDownloadClickA = () => {
+    window.location.href = "/amira.apk";
+  };
+
+  const handleDownloadClickL = () => {
     window.location.href = "/amira.apk";
   };
 
@@ -16,12 +20,14 @@ export default function Home() {
     <div className="relative bg-white flex flex-col items-center justify-center h-full font-sans">
       {/* Main Content */}
       <div className="z-10 flex flex-col items-center justify-center m-5">
-        <Image
-          src="/logo-full-colored.png"
-          alt="Download Image"
-        />
+        <h3 className="text-md">AMIRA E-COMMERCE</h3>
         <h4 className="text-black mt-1 text-sm">
-          Tekan <a className="text-pink-700 font-bold cursor-pointer" onClick={handleDownloadClick}>disini</a> apabila aplikasi tidak terunduh otomatis
+          Tekan <a className="text-pink-700 font-bold cursor-pointer" onClick={handleDownloadClickA}>disini</a>
+        </h4>
+        <div className="h-5"></div>
+        <h3 className="text-md">LOGISTIK</h3>
+        <h4 className="text-black mt-1 text-sm">
+          Tekan <a className="text-pink-700 font-bold cursor-pointer" onClick={handleDownloadClickL}>disini</a>
         </h4>
       </div>
     </div>
