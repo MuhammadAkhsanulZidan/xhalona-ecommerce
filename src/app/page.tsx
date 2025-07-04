d27d7531ce9b51630b2e4beac5bf3cfb1171748a
Muhammad Akhsanul Zidan <akhsanul.zidan25@gmail.com>
2025-07-04 19:07:47 +0700

    logistik

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,19 +13,19 @@ export default function Home() {
   };
 
   const handleDownloadClickL = () => {
-    window.location.href = "/amira.apk";
+    window.location.href = "/logistik.apk";
   };
 
   return (
     <div className="relative bg-white flex flex-col items-center justify-center h-full font-sans">
       {/* Main Content */}
       <div className="z-10 flex flex-col items-center justify-center m-5">
-        <h3 className="text-md">AMIRA E-COMMERCE</h3>
+        <h3 className="text-md text-black">AMIRA E-COMMERCE</h3>
         <h4 className="text-black mt-1 text-sm">
           Tekan <a className="text-pink-700 font-bold cursor-pointer" onClick={handleDownloadClickA}>disini</a>
         </h4>
         <div className="h-5"></div>
-        <h3 className="text-md">LOGISTIK</h3>
+        <h3 className="text-md text-black">LOGISTIK</h3>
         <h4 className="text-black mt-1 text-sm">
           Tekan <a className="text-pink-700 font-bold cursor-pointer" onClick={handleDownloadClickL}>disini</a>
         </h4>
