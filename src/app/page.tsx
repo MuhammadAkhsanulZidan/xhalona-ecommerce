e67e33233992435adda04250d13bebde157daa0c
Muhammad Akhsanul Zidan <92286610+MuhammadAkhsanulZidan@users.noreply.github.com>
2025-10-07 16:46:38 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,18 +16,27 @@ export default function Home() {
     window.location.href = "/logistik.apk";
   };
 
+  const handleDownloadClickC = () => {
+    window.location.href = "/coreerpai.apk";
+  };
+
+
   return (
     <div className="relative bg-white flex flex-col items-center justify-center h-full font-sans">
       {/* Main Content */}
       <div className="z-10 flex flex-col items-center justify-center m-5">
         <h3 className="text-md text-black">E-COMMERCE</h3>
         <h4 className="text-black mt-1 text-sm">
-          Tekan <button className="bg-green-500 border-none text-white font-bold cursor-pointer" onClick={handleDownloadClickA}>disini</button>
+          Tekan <button className="text-green-500 border-none text-white font-bold cursor-pointer" onClick={handleDownloadClickA}>disini</button>
         </h4>
         <div className="h-5"></div>
         <h3 className="text-md text-black">LOGISTIK</h3>
         <h4 className="text-black mt-1 text-sm">
-          Tekan <button className="bg-blue-500 border-none text-white font-bold cursor-pointer" onClick={handleDownloadClickL}>disini</button>
+          Tekan <button className="text-blue-500 border-none text-white font-bold cursor-pointer" onClick={handleDownloadClickL}>disini</button>
+        </h4>
+        <h3 className="text-md text-black">CORE ERP AI</h3>
+        <h4 className="text-black mt-1 text-sm">
+          Tekan <button className="text-blue-500 border-none text-white font-bold cursor-pointer" onClick={handleDownloadClickC}>disini</button>
         </h4>
       </div>
     </div>
